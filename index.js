0f2118cc10ba320eece0f315096b46d955e97875
Nashiba-Namutebi <154015977+Nashiba-Namutebi@users.noreply.github.com>
2026-04-18 10:00:37 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const restify = require('restify');
 const { BotFrameworkAdapter } = require('botbuilder');
 
-// Create server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log("Bot is running on port 3978");
+server.listen(process.env.PORT || 3978, () => {
+    console.log(`Bot running on port ${process.env.PORT || 3978}`);
 });
 
-// Create adapter
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
 
-// Listen for messages
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         if (context.activity.type === 'message') {
